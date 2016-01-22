5b5b230168799f3576fc9981f4e426bde1e06de9
Daniel Kehoe <daniel@danielkehoe.com>
2016-01-22 10:04:22 +0700

    admin doesn't need a profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate!(auth_options)
-    if current_user.profile.finished_profile?
-      set_flash_message(:notice, :signed_in) if is_flashing_format?
-    else
-      message = "Incomplete profile. Please complete your profile."
-      flash[:alert] = message if is_flashing_format?
+    unless current_user.admin?
+      if current_user.profile.finished_profile?
+        set_flash_message(:notice, :signed_in) if is_flashing_format?
+      else
+        message = "Incomplete profile. Please complete your profile."
+        flash[:alert] = message if is_flashing_format?
+      end
     end
     sign_in(resource_name, resource)
     yield resource if block_given?
