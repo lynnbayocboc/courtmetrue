fd57dc34244a6cc6aa5342e4d4aa1fb8c287b5fc
Daniel Kehoe <daniel@danielkehoe.com>
2016-02-25 18:54:43 +0800

    admin user gets a profile

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -5,5 +5,18 @@ class CreateAdminService
         user.password_confirmation = Rails.application.secrets.admin_password
         user.admin!
       end
+      profile = user.build_profile
+      profile.name = 'Admin User'
+      profile.gender = 'male'
+      profile.dob = Date.today - rand(20..28).years
+      profile.save
+      profile_photo = ProfilePhoto.new
+      uri = URI.parse(Faker::Avatar.image)
+      uri.scheme = 'https'
+      profile_photo.is_profile_pic = true
+      profile_photo.photo =  uri
+      profile_photo.profile = profile
+      profile_photo.save
+      return user
   end
 end
