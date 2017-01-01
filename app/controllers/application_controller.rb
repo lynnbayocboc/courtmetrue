e9430a733edf100adc18fef435116deeff6f11b3
Daniel Kehoe <daniel@danielkehoe.com>
2017-01-01 22:03:17 +0800

    update for Devise 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-	include ApplicationHelper
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  include ApplicationHelper
   def after_sign_in_path_for(resource)
     interests_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
