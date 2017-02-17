8e12b6d63b7af8f1412aaa9943ea82196e501d91
Cedric Matalog <cedricmatalog@gmail.com>
2017-02-17 21:59:56 +0000

    added dummy seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 #                                         { name: 'Road Trip' },
 #                                         { name: 'Doing Sports' }])
 
-users = User.create([{ email: 'admin@gmail.com', password: 'password', password_confirmation: 'password', role: 'admin' }])
-                    # { email: 'male@gmail.com', password: 'password', password_confirmation: 'password' },
-                    # { email: 'female@gmail.com', password: 'password', password_confirmation: 'password' }])
+users = User.create([{ email: 'admin@gmail.com', password: 'password', password_confirmation: 'password', role: 'admin' },
+                    { email: 'male@gmail.com', password: 'password', password_confirmation: 'password' },
+                    { email: 'female@gmail.com', password: 'password', password_confirmation: 'password' }])
                      
-# profiles = Profile.create([{user_id: 1, gender: "male", name: "admin", age: "20", city: "manila"},
-#                           {user_id: 2, gender: "male", name: "male", age: "20", city: "manila"},
-#                           {user_id: 3, gender: "female", name: "female", age: "20", city: "manila"}])
+profiles = Profile.create([{user_id: 1, gender: "male", name: "admin", age: "20", city: "manila"},
+                          {user_id: 2, gender: "male", name: "male", age: "20", city: "manila"},
+                          {user_id: 3, gender: "female", name: "female", age: "20", city: "manila"}])
 
 
 # user = CreateAdminService.new.call
